04af8421aeb9fc152f7639118d2647ebd63b66c0
Ulrich Schreck <ulrich.schreck@beck.de>
2022-09-21 09:16:45 +0200

    Add Date

--- vueApp.js
+++ vueApp.js
@@ -1,15 +1,32 @@
 const url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=&objectIds=191,236,310,239,317,234&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=GEN%2Ccases7_per_100k&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=none&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=standard&f=pjson&token=";
 
         const vm = new Vue({
-                el: '#app',
+              el: '#app',
+              data: {
+              results: []
+              },
+              mounted() {
+                axios.get(url).then(response => {
+                  this.results = response.data.features.map((result) => {
+                    return {GEN: result.attributes.GEN, cases: result.attributes.cases7_per_100k};
+                  }
+                )})
+                }
+            });
+        var vue_det = new Vue({
+              el: '#intro',
                 data: {
-                results: []
+                   timestamp: ""
                 },
-                mounted() {
-                  axios.get(url).then(response => {
-                    this.results = response.data.features.map((result) => {
-                        return {GEN: result.attributes.GEN, cases: result.attributes.cases7_per_100k};
+                created() {
+                    setInterval(this.getNow, 1000);
+                },
+                methods: {
+                    getNow: function() {
+                        const today = new Date();
+                        const date = today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear();
+                        this.timestamp = date;
                     }
-                  )})
-                 }
-              });
+                }
+             });
+        
